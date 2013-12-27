1b57b41f02f3894815d5c44eedb065f0934f0dfa
eric.zhang <zxq517@gmail.com>
2013-12-27 15:19:23 +0800

    cap 8

--- routes/index.js
+++ routes/index.js
@@ -268,6 +268,22 @@ module.exports = function(app) {
         });
     });
 
+    app.get('/archive', function (req, res) {
+        Post.getArchive(function (err, posts) {
+            if (err) {
+                req.flash('error', err);
+                return res.redirect('/');
+            }
+            res.render('archive', {
+                title: '存档',
+                posts: posts,
+                user: req.session.user,
+                success: req.flash('success').toString(),
+                error: req.flash('error').toString()
+            });
+        });
+    });
+
     function checkLogin(req, res, next) {
         if (!req.session.user) {
             req.flash('error', '未登录!');
